b2a13dbb838da887107a4431c3be4956c36bea25
Nate Petersen <nate@digitalrickshaw.com>
2022-01-14 10:08:50 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,133 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+
+function linkedIssuesAndProjects(pullRequestId) {
+  // We have it return info about all the linked projects and their fields,
+  // so that we can identify the field that needs to be updated.
+  
+  return `query {
+    node(id: "${pullRequestId}") {
+      ... on PullRequest {
+        id
+        title
+        number
+        closingIssuesReferences(first: 50) {
+          nodes {
+            id
+            number
+            title
+            projectsNext(first: 50) {
+              nodes {
+                id
+                title
+                fields(first: 50) {
+                  nodes {
+                    id
+                    name
+                    settings
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }`
+}
+
+function extractProjectItemId(projectId, itemId) {
+  // The id we need for the item is not it's own id, but rather the id of its association with the project.
+  // There is no way to query for that directly. So instead we tell GH to add the item to the project. This
+  // has no effect since it is part of it already, but it does return the needed id.
+
+  return `mutation {
+    addProjectNextItem(input: {projectId: "${projectId}", contentId:"${itemId}"}) {
+      projectNextItem {
+        id
+      }
+    }
+  }`
+}
+
+function updateProjectFieldMutation(projectId, projectItemId, fieldId, fieldValue) {
+  return `mutation {
+    updateProjectNextItemField(input: {projectId: "${projectId}", itemId: "${projectItemId}", fieldId: "${fieldId}", value: "${fieldValue}"}) {
+      clientMutationId
+    }
+  }`
+}
+
+async function main() {
+  try {
+    const pr = github.context.payload.pull_request;
+
+    if (!pr) {
+      console.log(`Payload doesn't contain a pull request, so nothing to be done.`);
+      return;
+    }
+
+    const token = core.getInput('github-token')
+    const projectFieldName = core.getInput('project-field-name')
+    const projectFieldValue = core.getInput('project-field-value')
+
+    const octokit = github.getOctokit(token);
+
+    // Find all issues this PR will close
+    const linkedIssuesQuery = linkedIssuesAndProjects(pr.node_id);
+    core.debug("query:", linkedIssuesQuery);
+
+    const { node } = await octokit.graphql(linkedIssuesQuery);
+    core.debug("node:", JSON.stringify(node, undefined, 2));
+
+    console.log(JSON.stringify(node, undefined, 2));
+
+    for (let issue of node.closingIssuesReferences.nodes) {
+      for (let project of issue.projectsNext.nodes) {
+        // Find the id of target field in this project
+        const field = project.fields.nodes.find(f => f.name === projectFieldName)
+
+        if (!field) {
+          console.log(`Issue #${issue.number} has a card on project ${project.name}, but there is no field named ${projectFieldName}, so it won't be moved.`);
+          continue;
+        }
+
+        // Find the id of the target value for this field
+        const settings = JSON.parse(field.settings)
+        const options = settings.options
+
+        const targetValue = options.find( v => v.name === projectFieldValue )
+
+        if (!targetValue) {
+          console.log(`Issue #${issue.number} has a card on project ${project.name}, but the field named ${projectFieldName} doesn't have an option ${projectFieldValue}, so it won't be moved.`);
+          continue;
+        }
+
+        // Extract the id of the association of this item to this project
+        const projectItemIdQuery = extractProjectItemId(project.id, issue.id)
+
+        core.debug("projectItemIdQuery:", projectItemIdQuery);
+
+        const { addProjectNextItem } = await octokit.graphql(projectItemIdQuery);
+
+        core.debug("addProjectNextItem:", JSON.stringify(addProjectNextItem, undefined, 2));
+
+        console.log(JSON.stringify(addProjectNextItem, undefined, 2));
+
+        // Make the actual change to the field
+        console.log(`Setting issue #${issue.number} field "${projectFieldName}" to "${projectFieldValue}" in project "${project.name}"`)
+
+        const updateMutation = updateProjectFieldMutation(project.id, addProjectNextItem.projectNextItem.id, field.id, targetValue.id)
+        
+        console.log(updateMutation)
+
+        await octokit.graphql(updateMutation)
+      }
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+main();
